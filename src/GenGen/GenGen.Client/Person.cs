693111a29fcef10c343b443999a6181da4053ad2
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2021-11-07 14:19:58 -0500

    Basic generator wired up

---
+++ src/GenGen/GenGen.Client/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoNotify;
+
+namespace GenGen.Client
+{
+    public partial class Person
+    {
+        [AutoNotify]
+        private string name = string.Empty;
+    }
+}
---
+++ src/GenGen/GenGen.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenGen.Client
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
