e95401694d497eb665ff9ec028c35d2b354b5694
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-01-16 16:44:04 -0500

    Clarify which code is which

--- src/ConsoleClient/GobieComponentsDev.cs
+++ src/ConsoleClient/GobieComponentsDev.cs
@@ -27,41 +27,6 @@
     { }
 
     /// <summary>
-    /// User provided generator definition.
-    /// </summary>
-    public class EncapulatedCollectionGenerator : GobieFieldGenerator
-    {
-        [GobieTemplate]
-        private const string EncapsulationTemplate =
-@"      public IEnumerable<string> {{ Property }} => {{ field }}.AsReadOnly();
-
-        public IEnumerable<int> {{ Property }}Lengths => {{ field }}.Select(x => x.Length);
-";
-
-        [GobieTemplate]
-        private const string AddMethod =
-@"      public bool TryAdd{{ Property }}(string s)
-        {
-            {{#CustomValidator}}
-            if({{CustomValidator}}(s))
-            {
-                {{ field }}.Add(s);
-                return true;
-            }
-            return false;
-            {{/CustomValidator}}
-            {{^CustomValidator}}
-            {{ field }}.Add(s);
-            return true;
-            {{/CustomValidator}}
-        }";
-
-        public override bool DebugGenerator { get; protected set; } = true;
-
-        public string CustomValidator { get; set; } = null;
-    }
-
-    /// <summary>
     /// Attribute produced by gobie. Defined in <see cref="EncapulatedCollectionGenerator"/>.
     /// </summary>
     public sealed class EncapulatedCollectionAttribute : GobieFieldGeneratorAttribute
---
+++ src/ConsoleClient/UserCode.cs
@@ -0,0 +1,44 @@
+﻿using Gobie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    /// <summary>
+    /// User provided generator definition.
+    /// </summary>
+    public class EncapulatedCollectionGenerator : GobieFieldGenerator
+    {
+        [GobieTemplate]
+        private const string EncapsulationTemplate =
+@"      public IEnumerable<string> {{ Property }} => {{ field }}.AsReadOnly();
+
+        public IEnumerable<int> {{ Property }}Lengths => {{ field }}.Select(x => x.Length);
+";
+
+        [GobieTemplate]
+        private const string AddMethod =
+@"      public bool TryAdd{{ Property }}(string s)
+        {
+            {{#CustomValidator}}
+            if({{CustomValidator}}(s))
+            {
+                {{ field }}.Add(s);
+                return true;
+            }
+            return false;
+            {{/CustomValidator}}
+            {{^CustomValidator}}
+            {{ field }}.Add(s);
+            return true;
+            {{/CustomValidator}}
+        }";
+
+        public override bool DebugGenerator { get; protected set; } = true;
+
+        public string CustomValidator { get; set; } = null;
+    }
+}
