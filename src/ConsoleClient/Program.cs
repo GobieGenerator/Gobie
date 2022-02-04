10ceff4c5c4f71bf9c116a1cd41cfec2987f0c3e
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-02-03 20:56:54 -0500

    hello verify

--- src/ConsoleClient/Program.cs
+++ src/ConsoleClient/Program.cs
@@ -9,18 +9,18 @@
         {
             var auth = new Author();
 
-            auth.TryAddBooks("Spellmonger");
-            auth.TryAddBooks("The Warrior's Apprentace");
+            ////auth.TryAddBooks("Spellmonger");
+            ////auth.TryAddBooks("The Warrior's Apprentace");
 
-            foreach (var book in auth.Books)
-            {
-                Console.WriteLine(book);
-            }
+            ////foreach (var book in auth.Books)
+            ////{
+            ////    Console.WriteLine(book);
+            ////}
 
-            foreach (var bl in auth.BooksLengths)
-            {
-                Console.WriteLine(bl);
-            }
+            ////foreach (var bl in auth.BooksLengths)
+            ////{
+            ////    Console.WriteLine(bl);
+            ////}
         }
     }
 }
---
+++ src/Gobie.UnitTests/SnapshotTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Gobie.UnitTests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using VerifyNUnit;
+
+    [TestFixture]
+    public class SnapshotTests
+    {
+        [Test]
+        public Task Test()
+        {
+            var person = new
+            {
+                Id = "ebced679-45d3-4653-8791-3d969c4a6c",
+                Title = "Mr.",
+                GivenNames = "Johnnyzzz",
+                FamilyName = "Smith",
+                Spouse = "Jill",
+                Children = new List<string> 
+                {
+                    "Sam",
+                    "Mary"
+                },
+                Address = new
+                {
+                    Street = "4 Puddle Lane",
+                    Country = "USA"
+                }
+            };
+            return Verifier.Verify(person);
+        }
+    }
+}
