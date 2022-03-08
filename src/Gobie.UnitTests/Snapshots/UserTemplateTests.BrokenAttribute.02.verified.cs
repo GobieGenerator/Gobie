3860b477db4748913aafd470b2f90462de42a74e
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-03-08 18:55:59 -0500

    Remove old files

--- src/Gobie.UnitTests/Snapshots/UserTemplateTests.BrokenAttribute.02.verified.cs
+++
@@ -1,9 +0,0 @@
-﻿//HintName: BrokenAttribute.g.cs
-namespace Gobie
-{
-    /// <summary> This attribute will cause the generator defined by this thing here to
-    /// run <see cref = "TODONAMESPACE.Broken"/> to run. </summary>
-    public sealed class BrokenAttribute : Gobie.GobieFieldGeneratorAttribute
-    {
-    }
-}
