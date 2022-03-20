9f3c12f7fa52b8fc184961abfb57495e87c6afe9
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-03-20 16:26:07 -0400

    Tokenizer works

---
+++ src/Gobie.UnitTests/MoustacheTokenizePropertyBasedTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Gobie.UnitTests;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Gobie.Mustache;
+
+[TestFixtureSource(nameof(FixtureArgs))]
+public class MoustacheTokenizePropertyBasedTests
+{
+    private static object[] FixtureArgs = {
+        new object[] { "Question"},
+        new object[] { "{{Answer}}" },
+        new object[] { "{{#name}}\r\n  <b>{{name}}</b>\r\n{{/name}}\r\n{{^name}}\r\n  No name. \r\n{{/name}}" },
+        new object[] { "  {{name. fjkd}}</b>\r\n{{" },
+    };
+
+    private readonly string template;
+    private readonly Token[] tokens;
+
+    public MoustacheTokenizePropertyBasedTests(string template)
+    {
+        this.template = template;
+        tokens = Mustache.Tokenize(this.template).ToArray();
+    }
+
+    [Test]
+    public void TokenNumberDoesntExceedTemplateLength() => Assert.IsTrue(template.Length >= tokens.Length);
+
+    [Test]
+    public void TokensArentNone() => Assert.AreEqual(0, tokens.Count(x => x.TokenType == TokenType.None));
+
+    [Test]
+    public void TokensMinPositionIsZero() => Assert.AreEqual(0, tokens.Min(x => x.Start));
+
+    [Test]
+    public void TokensMaxPositionIsTemplateLength() => Assert.AreEqual(template.Length - 1, tokens.Max(x => x.End));
+
+    [Test]
+    public void TokensHavePositiveLength() => Assert.AreEqual(0, tokens.Count(x => x.End < x.Start));
+
+    [Test]
+    public void TokensAreOrderedAndAdjacent()
+    {
+        for (int i = 0; i < tokens.Length - 1; i++)
+        {
+            Assert.AreEqual(tokens[i].End + 1, tokens[i + 1].Start);
+        }
+    }
+}
--- src/Gobie/Mustache.cs
+++ src/Gobie/Mustache.cs
@@ -9,11 +9,33 @@ using System.Threading.Tasks;
 
 public class Mustache
 {
-    public static void Tokenize(ReadOnlySpan<char> template)
+    public enum TokenType
+    {
+        None = 0,
+        Open,
+        Close,
+        LogicIfOpen,
+        LogicNotOpen,
+        LogicEndOpen,
+        General,
+        TemplateTokenOpen,
+
+        /// <summary>
+        /// This token is comprised of char that can be an identifier.
+        /// </summary>
+        Identifier,
+
+        Whitespace,
+        Period
+    }
+
+    public static ReadOnlySpan<Token> Tokenize(ReadOnlySpan<char> template)
     {
         var tokens = new Span<Token>(new Token[template.Length]);
 
-        int t = 0;
+        // Not we start at -1 so we can differentiate the first time we add a token from the first
+        // time we might be combining tokens.
+        int t = -1;
 
         for (int i = 0; i < template.Length; i++)
         {
@@ -21,36 +43,33 @@ public class Mustache
             if (TryMatch(tokens, template, "{{#", TokenType.LogicIfOpen)
                 || TryMatch(tokens, template, "{{^", TokenType.LogicNotOpen)
                 || TryMatch(tokens, template, "{{/", TokenType.LogicEndOpen)
+                || TryMatch(tokens, template, "{{", TokenType.TemplateTokenOpen)
                 || TryMatch(tokens, template, "}}", TokenType.Close))
             {
             }
             else if (template[i] == '.')
             {
-                AddOrCombine(new Token(i, i, TokenType.Period));
+                AddOrCombine(tokens, new Token(i, i, TokenType.Period));
             }
             else if (char.IsWhiteSpace(template[i]))
             {
-                AddOrCombine(new Token(i, i, TokenType.Whitespace));
+                AddOrCombine(tokens, new Token(i, i, TokenType.Whitespace));
             }
             else if (char.IsNumber(template[i])
                      || char.IsLetter(template[i])
                      || template[i] == '_')
             {
-                AddOrCombine(new Token(i, i, TokenType.General));
+                AddOrCombine(tokens, new Token(i, i, TokenType.Identifier));
             }
             else
             {
-                AddOrCombine(new Token(i, i, TokenType.General));
+                AddOrCombine(tokens, new Token(i, i, TokenType.General));
             }
-
-            
-
-            
 
             bool TryMatch(Span<Token> tokens, ReadOnlySpan<char> template, string compare, TokenType tokenType)
             {
                 var len = compare.Length;
-                if(i + len > template.Length)
+                if (i + len > template.Length)
                 {
                     return false;
                 }
@@ -66,27 +85,23 @@ public class Mustache
                     return false;
                 }
 
-                tokens[t] = new Token(i, i + len - 1, tokenType);
                 t++;
+                tokens[t] = new Token(i, i + len - 1, tokenType);
                 i += compare.Length - 1; // Advance, but keep in mind we are going to still get i++'
                 return true;
             }
         }
 
-        void AddOrCombine(Token token)
+        void AddOrCombine(Span<Token> tokens, Token token)
         {
-            if (t == 0)
+            if (t == -1)
             {
-                tokens[t] == token;
                 t++;
+                tokens[t] = token;
             }
-
-            if (token.TokenType == TokenType.None) continue;
-
-            if (token.TokenType is TokenType.Whitespace or TokenType.General &&
-                token.TokenType == tokens[t].TokenType)
+            else if (token.TokenType == tokens[t].TokenType)
             {
-                tokens[t] = new Token(token.Start, token.End + 1, token.TokenType);
+                tokens[t] = new Token(tokens[t].Start, token.End, tokens[t].TokenType);
             }
             else
             {
@@ -96,13 +111,12 @@ public class Mustache
             }
         }
 
-        var final = tokens.Slice(0, t + 1);
+        return tokens.Slice(0, t + 1);
     }
 
     [DebuggerDisplay("{Start}-{End} {TokenType}")]
     public readonly struct Token
     {
- 
         public Token(int start, int end, TokenType tokenType)
         {
             Start = start;
@@ -111,25 +125,9 @@ public class Mustache
         }
 
         public int Start { get; }
+
         public int End { get; }
+
         public TokenType TokenType { get; }
-    }
-
-    public enum TokenType
-    {
-        None = 0, 
-        Open,
-        Close,
-        LogicIfOpen,
-        LogicNotOpen,
-        LogicEndOpen,
-        General,
-
-        /// <summary>
-        /// This token is comprised of char that can be an identifier.
-        /// </summary>
-        Identifier,
-        Whitespace,
-        Period
     }
 }
