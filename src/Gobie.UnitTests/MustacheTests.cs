8d0f0b82b7e6ca538734b19eb4d2b0caaa426827
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-03-23 21:32:49 -0400

    Add failing test

--- src/Gobie.UnitTests/MustacheTests.cs
+++ src/Gobie.UnitTests/MustacheTests.cs
@@ -9,5 +9,10 @@ public class MustacheTests
         Verify(new ParseResult(template, Mustache.Parse(template)))
             .UseDirectory("Snapshots\\Mustache");
 
+    [TestCase("{{#name}}Something{{/name}} {{^name}}Something Else{{/name}}")]
+    public Task Parse_WithLogicFlatAst(string template) =>
+        Verify(new ParseResult(template, Mustache.Parse(template)))
+            .UseDirectory("Snapshots\\Mustache");
+
     private record ParseResult(string Template, DataOrDiagnostics<Mustache.TemplateSyntax> Result);
 }
