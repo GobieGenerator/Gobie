a78a7336b118f17af055cc9bce2487e96664fd9b
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-04-02 11:35:23 -0400

    Setup attribute project
    https://andrewlock.net/creating-a-source-generator-part-8-solving-the-source-generator-marker-attribute-problem-part2/

--- src/Gobie/Gobie.Attributes/AllAttributes.cs
+++ src/Gobie/Gobie.Attributes/AllAttributes.cs
@@ -40,13 +40,11 @@
     {
     }
 
-    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public abstract class GobieFieldGeneratorAttribute : Attribute
     {
     }
 
-    ////[GobieGeneratorName("something")]
-    ////[GobieGeneratorName("something", Namespace = "foo")]
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public sealed class GobieGeneratorNameAttribute : Attribute
     {
