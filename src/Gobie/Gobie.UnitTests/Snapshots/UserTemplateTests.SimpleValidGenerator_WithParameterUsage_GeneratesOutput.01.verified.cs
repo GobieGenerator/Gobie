a454f34aa84354d2257762b2025be4e3e7ea73d9
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-04-02 17:51:33 -0400

    Clear incorrect validation file

--- src/Gobie/Gobie.UnitTests/Snapshots/UserTemplateTests.SimpleValidGenerator_WithParameterUsage_GeneratesOutput.01.verified.cs
+++ src/Gobie/Gobie.UnitTests/Snapshots/UserTemplateTests.SimpleValidGenerator_WithParameterUsage_GeneratesOutput.01.verified.cs
@@ -1,8 +1,1 @@
-﻿//HintName: NamePropertyAttribute_TemplateTarget.g.cs
-namespace SomeNamespace
-{
-    public partial class TemplateTarget
-    {
-        public string Name { get; set; } = "";
-    }
-}
+﻿
