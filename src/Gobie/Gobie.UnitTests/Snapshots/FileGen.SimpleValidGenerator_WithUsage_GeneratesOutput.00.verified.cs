bdaa4b531b0a2c43c9e158a7c4c13f9e6a83cc94
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-04-17 13:53:44 -0400

    Correct file output

--- src/Gobie/Gobie.UnitTests/Snapshots/FileGen.SimpleValidGenerator_WithUsage_GeneratesOutput.00.verified.cs
+++ src/Gobie/Gobie.UnitTests/Snapshots/FileGen.SimpleValidGenerator_WithUsage_GeneratesOutput.00.verified.cs
@@ -1,11 +1,11 @@
-﻿//HintName: _Gobie.PrimaryKeyAttribute.g.cs
+﻿//HintName: _Gobie.LoggedClassAttribute.g.cs
 namespace Gobie
 {
     /// <summary> This attribute will cause the generator defined by this thing here to
     /// run <see cref = "Gobie.PrimaryKeyGenerator"/> to run. </summary>
-    public sealed class PrimaryKeyAttribute : global::Gobie.GobieClassGeneratorAttribute
+    public sealed class LoggedClassAttribute : global::Gobie.GobieClassGeneratorAttribute
     {
-        public PrimaryKeyAttribute()
+        public LoggedClassAttribute()
         {
         }
     }
---
+++ src/Gobie/Gobie.UnitTests/Snapshots/FileGen.SimpleValidGenerator_WithUsage_GeneratesOutput.01.verified.cs
@@ -0,0 +1,8 @@
+﻿//HintName: GenTarget_LoggedClassAttribute.g.cs
+namespace SomeNamespace
+{
+    public partial class GenTarget
+    {
+        private readonly List<GenTargetLog> logs = new();
+    }
+}
