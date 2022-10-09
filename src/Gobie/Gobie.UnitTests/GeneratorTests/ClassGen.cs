f5018495b7df663b27cbd79197d23254ddbf3848
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-10-09 13:23:38 -0400

    Class not sealed

--- src/Gobie/Gobie.UnitTests/GeneratorTests/ClassGen.cs
+++ src/Gobie/Gobie.UnitTests/GeneratorTests/ClassGen.cs
@@ -43,17 +43,6 @@ public class ClassGen
     }
 
     [Test]
-    public Task Partial_GetsDiagnostic()
-    {
-        var source = @"
-        using Gobie;
-
-        public partial sealed class UserDefinedGenerator : GobieClassGenerator" + TrivialTemplate;
-
-        return TestHelper.Verify(source);
-    }
-
-    [Test]
     public Task Unsealed_GetsDiagnostic()
     {
         var source = @"
---
+++ src/Gobie/Gobie.UnitTests/GeneratorTests/DiagnosticTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Gobie.UnitTests.GeneratorTests;
+
+/// <summary>
+/// Tests here are verifying particular line numbers. Both lines and columns are zero indexed.
+/// </summary>
+public class DiagnosticTests
+{
+    [Test]
+    public Task GB1005_UserGeneratorIsNotSealed()
+    {
+        var source = @"
+        public class UserDefinedGenerator : GobieClassGenerator
+        {
+            [GobieTemplate]
+            private const string EncapsulationTemplate = "";
+        }";
+
+        return TestHelper.Verify(source);
+    }
+}
