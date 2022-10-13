65193b4d44d56c2397995bbb94222b749056c3d1
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-10-13 09:27:12 -0400

    Add failing test

--- src/Gobie/Gobie.UnitTests/GeneratorTests/DiagnosticErrorsCorrectLocInTemplates.cs
+++ src/Gobie/Gobie.UnitTests/GeneratorTests/DiagnosticErrorsCorrectLocInTemplates.cs
@@ -68,4 +68,22 @@ public class DiagnosticErrorsCorrectLocInTemplates
 
         return TestHelper.Verify(source);
     }
+
+    [Test]
+    public Task BadTagStringWithLineBreaks()
+    {
+        // This isn't even recognized as containing a template. This has multiple string literal
+        // expressions one node down, below the 'add' expression.
+
+        var source = @"
+        public sealed class UserDefinedGenerator : Gobie.GobieClassGenerator
+        {
+            [GobieTemplate]
+            private const string EncapsulatedCollection =
+                   ""public System.Collections.Generic.IEnumerable<{{FieldGenericType}}>"" +
+                   ""{{FieldName : BADTAG}} => {{FieldName}}.AsReadOnly();"";
+        }";
+
+        return TestHelper.Verify(source);
+    }
 }
