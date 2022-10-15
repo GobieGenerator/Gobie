8ac0bddff98d856134afbae31d4250dee29d2352
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-10-15 14:27:49 -0400

    Remove dupe tests

--- src/Gobie/Gobie.UnitTests/GeneratorTests/GlobalGen.cs
+++ src/Gobie/Gobie.UnitTests/GeneratorTests/GlobalGen.cs
@@ -31,63 +31,6 @@ public class GlobalGen
     }
 
     [Test]
-    public Task SimpleValidGenerator_IdentifierNotChildContent_RaisesDiagnostic()
-    {
-        var source = @"
-        [assembly: EFCoreRegistration]
-        namespace SomeNamespace
-        {
-            using Gobie;
-            public sealed class EFCoreRegistrationGenerator : GobieGlobalGenerator
-            {
-                [GobieGlobalFileTemplate(""Log"", ""EFCoreRegistration"")]
-                private const string KeyString = @""
-                namespace SomeNamespace;
-
-                public sealed static class EFCoreRegistration
-                {
-                    public static void Register()
-                    {
-                        {{OtherContent}}
-                    }
-                }
-                "";
-            }
-        }";
-
-        return TestHelper.Verify(source);
-    }
-
-    [Test]
-    public Task SimpleValidGenerator_MultipleChildContent_RaisesDiagnostic()
-    {
-        var source = @"
-        [assembly: EFCoreRegistration]
-        namespace SomeNamespace
-        {
-            using Gobie;
-            public sealed class EFCoreRegistrationGenerator : GobieGlobalGenerator
-            {
-                [GobieGlobalFileTemplate(""Log"", ""EFCoreRegistration"")]
-                private const string KeyString = @""
-                namespace SomeNamespace;
-
-                public sealed static class EFCoreRegistration
-                {
-                    public static void Register()
-                    {
-                        {{ChildContent}}
-                        {{ChildContent}}
-                    }
-                }
-                "";
-            }
-        }";
-
-        return TestHelper.Verify(source);
-    }
-
-    [Test]
     public Task SimpleValidGenerator_WithUsageNoIdentifiers_GeneratesOutput()
     {
         var source = @"
