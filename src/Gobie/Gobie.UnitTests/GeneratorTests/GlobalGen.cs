14cdb326cd4d905ce79795a86d7dfd5326787784
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-10-16 15:26:10 -0400

    Test multiple global generators work

--- src/Gobie/Gobie.UnitTests/GeneratorTests/GlobalGen.cs
+++ src/Gobie/Gobie.UnitTests/GeneratorTests/GlobalGen.cs
@@ -92,7 +92,7 @@ public class GlobalGen
         return TestHelper.Verify(source);
     }
 
-    [Ignore("Known to be failing")]
+    [Ignore("Failing because of the namespace?")]
     [Test]
     public Task SimpleValidGenerator_WithUsageAndIdentifiers_AttrUsesNamespace_GeneratesOutput()
     {
@@ -176,6 +176,89 @@ public class GlobalGen
             {
                 [EncapsulatedCollection]
                 private readonly List<string> names = new();
+            }
+        }";
+
+        return TestHelper.Verify(source);
+    }
+
+    [Test]
+    public Task TwoValidGenerators_WithUsageAndIdentifiers_References_GeneratesOutput()
+    {
+        var source = @"
+        [assembly: EFCoreRegistration]
+        [assembly: OtherGlobal]
+        namespace SomeNamespace
+        {
+            // ============= First Generator ============
+            using Gobie;
+            public sealed class EFCoreRegistrationGenerator : GobieGlobalGenerator
+            {
+                [GobieGlobalFileTemplate(""EFCoreRegistration"")]
+                private const string KeyString = @""
+                namespace SomeNamespace;
+
+                public sealed static class EFCoreRegistration
+                {
+                    public static void Register()
+                    {
+                        {{#ChildContent}}
+                            // Global generator code
+                            {{ChildContent}}
+                        {{/ChildContent}}
+                        {{^ChildContent}}
+                            // No generators reference this global generator.
+                        {{/ChildContent}}
+                    }
+                }
+                "";
+            }
+
+            public sealed class EncapsulatedCollectionGenerator : GobieFieldGenerator
+            {
+                [GobieTemplate]
+                private const string ReadonlyCollection = ""public IEnumerable<{{FieldGenericType}}> {{FieldName : pascal}} => {{FieldName}}.AsReadOnly(); // Encapsulating {{FieldType}}"";
+
+                [GobieGlobalChildTemplate(""EFCoreRegistration"")]
+                private const string EfCoreRegistration = ""// Hello EFCoreRegistration From {{ClassName}}.{{FieldName}}"";
+
+                [GobieGlobalChildTemplate(""OtherGlobal"")]
+                private const string OtherRegistration = ""// Hello OtherGlobal From {{ClassName}}.{{FieldName}}"";
+            }
+
+            public partial class TemplateTarget
+            {
+                [EncapsulatedCollection]
+                private readonly List<string> names = new(), addresses = new(), books = new();
+            }
+
+            public partial class OtherTarget
+            {
+                [EncapsulatedCollection]
+                private readonly List<string> names = new();
+            }
+
+            // ============= Second Generator ============
+            public sealed class OtherGlobalGenerator : GobieGlobalGenerator
+            {
+                [GobieGlobalFileTemplate(""OtherGlobal"")]
+                private const string KeyString = @""
+                namespace SomeNamespace;
+
+                public sealed static class OtherGlobal
+                {
+                    public static void Register()
+                    {
+                        {{#ChildContent}}
+                            // Global generator code
+                            {{ChildContent}}
+                        {{/ChildContent}}
+                        {{^ChildContent}}
+                            // No generators reference this global generator.
+                        {{/ChildContent}}
+                    }
+                }
+                "";
             }
         }";
 
