60b8c5649f276550e0bebcf1bb42080b0a00358d
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-11-13 15:49:01 -0500

    Start adding integration tests

---
+++ src/Gobie/Gobie.IntegrationTests/Class1.cs
@@ -0,0 +1,48 @@
+﻿namespace Gobie.IntegrationTests;
+
+using Gobie;
+using NUnit.Framework;
+
+public sealed class EncapsulatedCollectionGenerator : GobieFieldGenerator
+{
+    [GobieTemplate]
+    public const string Temp = """
+        // Some comment
+        public IEnumerable<{{FieldGenericType}}>  {{FieldName : pascal }} => {{FieldName}}.AsReadOnly();
+
+         public void Add{{ FieldName : pascal }}({{FieldGenericType}} s)
+        {
+            if(s is null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
+            {{#CustomValidator}}
+            if({{CustomValidator}}(s))
+            {
+                {{FieldName}}.Add(s);
+            }
+            {{/CustomValidator}}
+
+            {{^CustomValidator}}
+                {{FieldName}}.Add(s);
+            {{/CustomValidator}}
+        }
+        """;
+
+    public string CustomValidator { get; set; } = null!;
+}
+
+public partial class Encapsulation
+{
+    [EncapsulatedCollection]
+    private List<string> authors = new();
+}
+
+public class Class1
+{
+    [Test]
+    public void Test()
+    {
+    }
+}
