520f7dba6ae9c8547a96a9641cf2713e65886248
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2023-08-06 15:20:09 -0400

    Test passing

--- src/Gobie/test/Gobie.UnitTests/Snapshots/DiagnosticErrors.GB1024_InvalidTag.01.verified.cs
+++ src/Gobie/test/Gobie.UnitTests/Snapshots/DiagnosticErrors.GB1024_InvalidTag.01.verified.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//HintName: _Gobie.UserDefinedAttribute.g.cs
+namespace Gobie
+{
+    /// <summary>
+    /// This attribute will cause the generator <see 
+    ///cref = "Gobie.UserDefinedGenerator"/> to run.
+    /// </summary>
+    public sealed class UserDefinedAttribute : global::Gobie.GobieClassGeneratorAttribute
+    {
+        public UserDefinedAttribute()
+        {
+        }
+    }
+}
